032266270b2745898e8cc1eb187eea76ba0b4f71
Jongil Yoon <jiysait@gmail.com>
2021-05-09 10:19:15 -0600

    django + djongo + mongodb basic connection established

--- src/bio_back/settings.py
+++ src/bio_back/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'bio_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'learning',
+        'CLIENT': {
+            'host': 'mongo',
+            'port': 27017,
+            'username': 'countonme',
+            'password': 'qhduwnfRp5115db',
+            # 'authSource': 'amin'
+        }
     }
 }
 
