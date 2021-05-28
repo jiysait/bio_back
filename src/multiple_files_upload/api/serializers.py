10dda8b4b5f3d622677ba0ea259ac5fd5503cc0a
Jongil Yoon <jiysait@gmail.com>
2021-05-28 10:41:09 -0600

    multiple file upload is working

--- src/multiple_files_upload/api/serializers.py
+++ src/multiple_files_upload/api/serializers.py
@@ -1,10 +1,17 @@
+from django.db.models import fields
 from rest_framework import serializers
 
 from multiple_files_upload.models import MyFile
 
 
-class MultipleFilesUploadSerializer(serializers.ModelSerializer):
+class MultipleFilesUploadSerializer(serializers.Serializer):
 
-    class Meta:
-        model = MyFile
-        fields = '__all__'
+    files = serializers.ListField(child=serializers.FileField(
+        max_length=1000000, allow_empty_file=False, use_url=False))
+
+    def create(self, validated_data):
+        results = []
+        files = validated_data.pop('files')
+        for afile in files:
+            results.append(MyFile.objects.create(file=afile))
+        return results
--- src/multiple_files_upload/api/views.py
+++ src/multiple_files_upload/api/views.py
@@ -1,4 +1,3 @@
-import json
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, parser_classes
@@ -7,19 +6,12 @@ from rest_framework.parsers import MultiPartParser
 from .serializers import MultipleFilesUploadSerializer
 
 
-# You can choose one of approaches to handle HTTP Requests
-
-# Function Based Views
-# This approach is not commonly used, but it is much more straightforward.
 @api_view(['POST', ])
 @parser_classes([MultiPartParser])
 def multiple_files_upload_view(request):
     if request.method == 'POST':
-        serializer = MultipleFilesUploadSerializer(
-            data=request.data, many=True)
-        if serializer.is_valid():
+        serializer = MultipleFilesUploadSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
-
-# Class Based Views
